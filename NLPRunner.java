71a74c55332f16e01c1ee0aa5e64a929d678132b
ryangithub43 <rmoll001@mymail.lausd.net>
2025-03-05 14:43:53 -0800

    Create NLPRunner.java

---
+++ NLPRunner.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList; 
+
+public class NLPRunner {
+  public static void main(String[] args) {
+
+    ArrayList<String> username = new ArrayList<String>(); 
+
+    System.out.println(username); 
+
+    UserGiver ug = new UserGiver(); 
+
+    ug.prompt(); 
+
+
+
+    
+    
+    
+    
+  }
+}
